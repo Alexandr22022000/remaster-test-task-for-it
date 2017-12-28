9e4c2aa924c7fec5d1a4bc08eecaddba573992d0
Alexandr Zolotov <zol22222@mail.ru>
2017-12-28 13:32:11 +0700

    Fix compression

--- config/prod.config.js
+++ config/prod.config.js
@@ -2,7 +2,10 @@ const config = require('./common.config'),
   ExtractTextPlugin = require("extract-text-webpack-plugin"),
   OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
   UglifyJsPlugin = require('uglifyjs-webpack-plugin'),
-  CompressionPlugin = require("compression-webpack-plugin");
+  CompressionPlugin = require("compression-webpack-plugin"),
+  helpers = require('./helpers');
+
+config.entry['application.min'] = helpers.root('/source/application.ts');
 
 config.module.rules.push({
   test: /\.sass$/,
@@ -14,17 +17,16 @@ config.module.rules.push({
 
 config.plugins = [
   new UglifyJsPlugin({
-    include: /\.min\.js$/,
-    //minimize: true
+    include: /\.min$/
   }),
   new ExtractTextPlugin({filename: "styles.min.css"}),
   new OptimizeCssAssetsPlugin({
-    assetNameRegExp: /\.min\.css$/g,
+    assetNameRegExp: /\.min.css$/g,
     cssProcessor: require('cssnano'),
     cssProcessorOptions: { discardComments: {removeAll: true } },
     canPrint: true
   }),
-  new CompressionPlugin({test: /\.js$|\.min.css$/})
+  new CompressionPlugin({test: /\.min.js$|\.min.css$/})
 ];
 
 module.exports = config;
